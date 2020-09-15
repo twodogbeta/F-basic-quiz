d06b90a5d1c90b6f04b9c0367dfce8e2ed88ff05
twodogbeta <qijinhaoup@163.com>
2020-09-15 20:25:33 +0800

    [qjh] feat: 在后端拿到数据之后更新 header ，测试getUserBasicInformation方法(resolve 和 reject 两种情况)

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,3 +1,25 @@
-test("add 1+1 is 2", () => {
-  expect(2).toBe(2);
+import axios from "axios";
+import { getUserBasicInformation } from "../app";
+
+jest.mock("axios");
+
+describe(" can not get user basic information", () => {
+  test("should not get users basic information data", async () => {
+    axios.get.mockRejectedValue({});
+    await expect(getUserBasicInformation()).rejects.toEqual({});
+  });
+});
+describe(" get user basic information", () => {
+  test("should get users basic information data", async () => {
+    const mockdata = {
+      id: 1,
+      name: "KAMIL",
+      age: 24,
+      avatar: "https://inews.gtimg.com/newsapp_match/0/3581582328/0",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, non, dolorem, cumque distinctio magni quam expedita velit laborum sunt amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum! Quasi.",
+    };
+    axios.get.mockResolvedValue({ data: mockdata });
+    await expect(getUserBasicInformation()).resolves.toEqual(mockdata);
+  });
 });
--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 import "./style/index.scss";
+import axios from "axios";
+
+// parse data
+async function getUserBasicInformation(id) {
+  const response = await axios.get(`http://localhost:8080/users/${id}`);
+  return response.data;
+}
+
+async function displayFrontEndUserBasicInformation(id) {
+  const data = await getUserBasicInformation(id);
+  // $("#user-avatar").attr("src", data.avatar);
+  document.getElementById("user-avatar").src = data.avatar;
+  document.getElementById("user-name").innerHTML = data.name;
+  document.getElementById("user-age").innerHTML = data.age;
+}
+
+const url = window.location.href;
+const id = url.split("users/")[1];
+displayFrontEndUserBasicInformation(id);
+export { getUserBasicInformation };
